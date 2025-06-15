a6ae12dbd2f748637e309b2aa75b3a9215945a75
Dmytro Zubenko <fokist@outlook.com>
2025-06-16 02:06:43 +0300

    horrible test added

--- src/main/java/com/google/googlekeep/pages/KeepNotePage.java
+++ src/main/java/com/google/googlekeep/pages/KeepNotePage.java
@@ -2,6 +2,12 @@ package com.google.googlekeep.pages;
 
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class KeepNotePage {
     private final By skipWelcomeButton = By.id("com.google.android.keep:id/skip_welcome");
@@ -13,11 +19,31 @@ public class KeepNotePage {
 
     private final By cancelButton = By.id("android:id/button2");
     private final By listNoteButton = By.id("com.google.android.keep:id/new_list_button");
+    private final By listOfNotesTitle = By.xpath("//android.widget.TextView[@resource-id='com.google.android.keep:id/index_note_title']");
 
     private final AppiumDriver driver;
 
     public KeepNotePage(AppiumDriver driver) {
         this.driver = driver;
+    }
+
+
+    public void findNotesTitle(String targetNote) {
+        List<WebElement> notes = driver.findElements(listOfNotesTitle);
+        notes.stream()
+                .filter(note -> note.getText().equals(targetNote))
+                .findFirst()
+                .ifPresent(WebElement::click);
+    }
+
+    public boolean waitForNoteToAppear(String titleText, int timeoutSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutSeconds));
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.TextView[@text='" + titleText + "']")));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     public KeepNotePage skipWelcome() {
