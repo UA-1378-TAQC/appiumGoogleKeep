70b24e40e6c0bc2f3deb45856fe9b3ed2fdf5070
Liubomyr Halamaha <galamagal86@gmail.com>
2025-06-25 14:14:09 +0300

    Update HeaderEditorToolbarComponent.java

--- src/main/java/com/google/googlekeep/components/HeaderEditorToolbarComponent.java
+++ src/main/java/com/google/googlekeep/components/HeaderEditorToolbarComponent.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 
 public class HeaderEditorToolbarComponent extends Base {
 
-    private final By backButton = By.xpath("//android.widget.ImageButton[@content-desc=\"Перейти вгору\"]");
+    private final By backButton = By.xpath("//android.widget.ImageButton[@content-desc='Open navigation drawer']");
     private final By pinButton = By.id("com.google.android.keep:id/menu_pin");
     private final By reminderButton = By.id("com.google.android.keep:id/menu_reminder");
     private final By archiveButton = By.id("com.google.android.keep:id/menu_archive");
